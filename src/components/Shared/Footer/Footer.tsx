5cbfc890949a48caaf7b4f6c3647214ea48e8837
rakib8680 <rakib.cst.1st@gmail.com>
2024-04-30 18:21:39 +0600

    Add social media icons and copyright information to the footer

--- src/components/Shared/Footer/Footer.tsx
+++ src/components/Shared/Footer/Footer.tsx
@@ -1,11 +1,16 @@
 import { Box, Container, Stack, Typography } from "@mui/material";
+import Image from "next/image";
 import Link from "next/link";
+import facebookIcon from "@/assets/landing_page/facebook.png";
+import instagramIcon from "@/assets/landing_page/instagram.png";
+import twitterIcon from "@/assets/landing_page/twitter.png";
+import linkedInIcon from "@/assets/landing_page/linkedin.png";
 
 const Footer = () => {
   return (
     <Box bgcolor="rgb(17 26 34)" py={5}>
       <Container>
-        <Stack direction="row"  justifyContent='center' gap={4}>
+        <Stack direction="row" justifyContent="center" gap={4}>
           <Typography color="#fff" component={Link} href="/consultation">
             Consultation
           </Typography>
@@ -20,6 +25,41 @@ const Footer = () => {
           </Typography>
           <Typography color="#fff" component={Link} href="/ngo">
             NGOs
+          </Typography>
+        </Stack>
+        <Stack direction="row" justifyContent="center" gap={2} py={3}>
+          <Image src={facebookIcon} width={30} height={30} alt="facebook" />
+          <Image src={instagramIcon} width={30} height={30} alt="instagram" />
+          <Image src={twitterIcon} width={30} height={30} alt="twitter" />
+          <Image src={linkedInIcon} width={30} height={30} alt="linkedIn" />
+        </Stack>
+        <div className="border-b-[1px] border-dashed"></div>
+
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          gap={2}
+          py={3}
+        >
+          <Typography component="p" color="white">
+            © 2024 PH Health Care. All rights reserved
+          </Typography>
+          <Typography
+            variant="h4"
+            component={Link}
+            href="/"
+            fontWeight={600}
+            color="white"
+          >
+            P
+            <Box component="span" color="primary.main">
+              H
+            </Box>{" "}
+            Health Care
+          </Typography>
+          <Typography component="p" color="white">
+            Privacy Policy | Terms & Conditions
           </Typography>
         </Stack>
       </Container>
