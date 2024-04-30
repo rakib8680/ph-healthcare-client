e078671ee15cb32f720b4ecbe1301e641918be72
rakib8680 <rakib.cst.1st@gmail.com>
2024-04-30 17:04:20 +0600

    Add images remotePatterns to next.config.js and update global styles and theme

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>This is NotFoundPage component</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
---
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+
+
+
+
+const Navbar = () => {
+  return (
+     <div>
+         <h1>This is Navbar component</h1>
+     </div>
+  )
+};
+
+export default Navbar;
