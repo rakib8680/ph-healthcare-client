62d0dd9ed3ba86045f112513c18901c604bc9611
rakib8680 <rakib.cst.1st@gmail.com>
2024-04-30 20:36:55 +0600

    Hero Section Right SIde complete
    full hero section done

--- src/components/Ui/HomePage/HeroSection/HeroSection.tsx
+++ src/components/Ui/HomePage/HeroSection/HeroSection.tsx
@@ -47,7 +47,6 @@ const HeroSection = () => {
           variant="h6"
           component="p"
           sx={{
-            width: "45%",
             my: 4,
           }}
         >
@@ -67,7 +66,85 @@ const HeroSection = () => {
       </Box>
 
       {/* right box  */}
-      <Box>Right</Box>
+      <Box
+        sx={{
+          p: 1,
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          position: "relative",
+          mt: 0,
+        }}
+      >
+        {/* arrow  */}
+        <Box
+          sx={{
+            position: "absolute",
+            left: "300px",
+            top: "-30px",
+          }}
+        >
+          <Image src={assets.svgs.arrow} width={100} height={100} alt="arrow" />
+        </Box>
+
+        {/* doctor 1 & 2  */}
+        <Box
+          sx={{
+            display: "flex",
+            gap: 2,
+          }}
+        >
+          <Box mt={4}>
+            <Image
+              src={assets.images.doctor1}
+              width={240}
+              height={380}
+              alt="doctor1"
+            />
+          </Box>
+          <Box>
+            <Image
+              src={assets.images.doctor2}
+              width={240}
+              height={350}
+              alt="doctor2"
+            />
+          </Box>
+        </Box>
+
+        {/* doctor3  */}
+        <Box
+          sx={{
+            position: "absolute",
+            top: "230px",
+            left: "230px",
+          }}
+        >
+          <Image
+            src={assets.images.doctor3}
+            width={240}
+            height={240}
+            alt="doctor3"
+          />
+        </Box>
+
+        {/* stethoscope  */}
+        <Box
+          sx={{
+            position: "absolute",
+            zIndex: -1,
+            bottom: "-55px",
+            right: "80px",
+          }}
+        >
+          <Image
+            src={assets.images.stethoscope}
+            width={190}
+            height={190}
+            alt="stethoscope"
+          />
+        </Box>
+      </Box>
     </Container>
   );
 };
