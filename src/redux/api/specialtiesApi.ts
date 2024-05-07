9f10d2c5b329dce01a82e5178581c519dde2c214
rakib8680 <rakib.cst.1st@gmail.com>
2024-05-07 19:48:35 +0600

    Add Api End Point For Create Specialty

---
+++ src/redux/api/specialtiesApi.ts
@@ -0,0 +1,20 @@
+import { baseApi } from "./baseApi";
+
+
+
+
+
+const specialtiesApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    createSpecialty: build.mutation({
+      query: (data) => ({
+        url: "/specialties",
+        method: "POST",
+        contentType: "multipart/form-data",
+        data,
+      }),
+    }),
+  }),
+});
+
+export const { useCreateSpecialtyMutation } = specialtiesApi;
