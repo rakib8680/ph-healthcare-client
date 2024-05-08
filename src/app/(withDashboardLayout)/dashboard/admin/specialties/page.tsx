bee93197f41f768b76f44c0afd13d9df6f1b46f6
rakib8680 <rakib.cst.1st@gmail.com>
2024-05-08 18:39:53 +0600

    Exploring RenderCell Functionality In Data Grid Table

--- src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/admin/specialties/page.tsx
@@ -1,32 +1,58 @@
 "use client";
-import { Box, Button, CircularProgress, Stack, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  CircularProgress,
+  IconButton,
+  Stack,
+  TextField,
+} from "@mui/material";
 import SpecialtiesModal from "./components/SpecialtiesModal";
 import { useState } from "react";
 import { useGetAllSpecialtiesQuery } from "@/redux/api/specialtiesApi";
 import { DataGrid, GridColDef } from "@mui/x-data-grid";
-
-const columns = [
-  { field: "title", headerName: "Title", width: 100 },
-  {
-    field: "icon",
-    headerName: "Icon",
-    width: 100,
-    renderCell: ({ row }) => {
-      // console.log(row);
-    },
-  },
-];
+import Image from "next/image";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const SpecialtiesPage = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
 
-  const {
-    data: AllSpecialties,
-    isError,
-    isLoading,
-  } = useGetAllSpecialtiesQuery({});
+  const { data: AllSpecialties, isLoading } = useGetAllSpecialtiesQuery({});
 
-  // console.log(AllSpecialties);
+
+  const handleDelete = (id:string) =>{
+    console.log(id);
+  }
+
+
+  //columns
+  const columns: GridColDef[] = [
+    { field: "title", headerName: "Title", width: 300 },
+    {
+      field: "icon",
+      headerName: "Icon",
+      width: 300,
+      renderCell: ({ row }) => {
+        return (
+          <Box>
+            <Image src={row?.icon} alt="icon" width={30} height={30} />
+          </Box>
+        );
+      },
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 400,
+      renderCell: ({row}) => {
+        return (
+          <IconButton aria-label="delete" onClick={()=>handleDelete(row.id)}>
+            <DeleteIcon />
+          </IconButton>
+        );
+      },
+    },
+  ];
 
   return (
     <Box>
@@ -47,7 +73,7 @@ const SpecialtiesPage = () => {
           <CircularProgress />
         </Box>
       ) : (
-        <Box>
+        <Box my={3}>
           <DataGrid rows={AllSpecialties} columns={columns} />
         </Box>
       )}
