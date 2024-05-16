26fb6565e2bbe8c4f3480c25961020d41b3791aa
rakib8680 <rakib.cst.1st@gmail.com>
2024-05-16 17:25:35 +0600

    Update Doctor Data Functionality Implemented

--- src/app/(withDashboardLayout)/dashboard/doctor/profile/components/ProfileUpdateModal.tsx
+++ src/app/(withDashboardLayout)/dashboard/doctor/profile/components/ProfileUpdateModal.tsx
@@ -2,13 +2,14 @@ import PHForm from "@/components/Forms/PHForm";
 import PHInput from "@/components/Forms/PHInput";
 import PHSelectField from "@/components/Forms/PHSelectField";
 import PHFullScreenModal from "@/components/Shared/PHModal/PHFullScreenModal";
-import { useGetDoctorQuery } from "@/redux/api/doctorApi";
+import { useGetDoctorQuery, useUpdateDoctorMutation } from "@/redux/api/doctorApi";
 import { Gender } from "@/types";
 import { Button, Grid } from "@mui/material";
 import { FieldValues } from "react-hook-form";
 import MultipleSelectChip from "./MultipleSelectChip";
 import { useState } from "react";
 import { useGetAllSpecialtiesQuery } from "@/redux/api/specialtiesApi";
+import { toast } from "sonner";
 
 
 
@@ -37,11 +38,10 @@ type TProps = {
 // });
 
 const ProfileUpdateModal = ({ open, setOpen, id }: TProps) => {
-   const { data: doctorData, refetch, isSuccess } = useGetDoctorQuery(id);
-   const { data: allSpecialties } = useGetAllSpecialtiesQuery(undefined);
    const [selectedSpecialtiesIds, setSelectedSpecialtiesIds] = useState([]);
-
-//    const [updateDoctor, { isLoading: updating }] = useUpdateDoctorMutation();
+   const { data: doctorData, refetch } = useGetDoctorQuery(id);
+   const { data: allSpecialties } = useGetAllSpecialtiesQuery(undefined);
+   const [updateDoctor, { isLoading: updating }] = useUpdateDoctorMutation();
 
 
 // console.log(doctorData);
@@ -50,18 +50,67 @@ const ProfileUpdateModal = ({ open, setOpen, id }: TProps) => {
 
    const submitHandler = async (values: FieldValues) => {
 
-      console.log(values); 
+      values.experience = Number(values.experience);
+      values.apointmentFee = Number(values.apointmentFee);
+
       const specialties = selectedSpecialtiesIds.map(
          (specialtiesId: string) => ({
             specialtiesId,
             isDeleted: false,
          })
       );
+
+
+      const excludedFields: Array<keyof typeof values> = [
+         'email',
+         'id',
+         'role',
+         'needPasswordChange',
+         'status',
+         'createdAt',
+         'updatedAt',
+         'isDeleted',
+         'averageRating',
+         'review',
+         'profilePhoto',
+         'registrationNumber',
+         'schedules',
+         'doctorSpecialties',
+      ];
+
+
+      // at first this function will make an array of key value pairs and then filter out the excluded fields, then it will convert the array back to object
+      const filteredValues = Object.fromEntries(Object.entries(values).filter(([key])=>{
+         return !excludedFields.includes(key);
+      }))
+
+      filteredValues.specialties = specialties;
+
+
+      const payload = {
+         id,
+         body: filteredValues,
+     } 
+
+
+      try {
+        const res =await updateDoctor(payload).unwrap();
+          console.log(res);
+        if(res?.id){
+        await refetch();
+         toast.success("Doctor updated successfully!!!")
+         }
+         setOpen(false);
+      } catch (error) {
+         console.log(error);
+      }
+
+
     }
 
-    //   console.log({ id });
-      // return;
 
+    
+ 
 
 
    return (
@@ -169,7 +218,7 @@ const ProfileUpdateModal = ({ open, setOpen, id }: TProps) => {
                </Grid>
             </Grid>
 
-            <Button type='submit' >
+            <Button type='submit' disabled={updating} >
                Save
             </Button>
          </PHForm>
