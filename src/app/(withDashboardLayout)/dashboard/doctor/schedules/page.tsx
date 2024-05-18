c638f563954059bcc2a1258405c37a79b05012b6
rakib8680 <rakib.cst.1st@gmail.com>
2024-05-18 14:36:47 +0600

    Refactored Doctor Profile And Added Pagination To Doctor Schedule

--- src/app/(withDashboardLayout)/dashboard/doctor/schedules/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/doctor/schedules/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box, Button, IconButton } from "@mui/material";
+import { Box, Button, IconButton, Pagination } from "@mui/material";
 import DoctorScheduleModal from "./components/DoctorScheduleModal";
 import { useEffect, useState } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -12,24 +12,37 @@ import {
 } from "@/redux/api/doctorScheduleApi";
 import { dateFormatter } from "@/utils/dateFormatter";
 import dayjs from "dayjs";
-
-
+import AddIcon from '@mui/icons-material/Add';
 
 
 const DoctorSchedules = () => {
 
-   // states 
+
+  // states
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [allSchedule, setAllSchedule] = useState<any>([]);
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(4);
 
+
+  // queries  
+  const query: Record<string, any> = {};
+  query["page"] = page;
+  query["limit"] = limit;
 
 
   //   API call
-  const { data, isLoading } = useGetAllDoctorSchedulesQuery({});
+  const { data, isLoading } = useGetAllDoctorSchedulesQuery({ ...query });
   const [deleteDoctorSchedule] = useDeleteDoctorScheduleMutation();
+
+
 
   const doctorSchedules = data?.doctorSchedules;
   const meta = data?.meta;
+  let pageCount: number;
+  if (meta?.total) {
+    pageCount = Math.ceil(meta.total / limit);
+  }
 
 
 
@@ -45,7 +58,6 @@ const DoctorSchedules = () => {
     });
     setAllSchedule(updatedData);
   }, [doctorSchedules]);
-
 
 
 
@@ -85,20 +97,51 @@ const DoctorSchedules = () => {
     },
   ];
 
+
+
+  // Pagination
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
   
+
 
   return (
     <Box>
-      <Button onClick={() => setIsModalOpen(true)}>
+      <Button onClick={() => setIsModalOpen(true)} endIcon={<AddIcon/>}>
         Create Doctor Schedule
       </Button>
       <DoctorScheduleModal open={isModalOpen} setOpen={setIsModalOpen} />
       <Box sx={{ mb: 5 }}></Box>
-
       <Box>
         {!isLoading ? (
           <Box my={2}>
-            <DataGrid rows={allSchedule ?? []} columns={columns} />
+            <DataGrid
+              rows={allSchedule ?? []}
+              columns={columns}
+              hideFooterPagination
+              slots={{
+                // pagination 
+                footer: () => {
+                  return (
+                    <Box
+                      sx={{
+                        mb: 2,
+                        display: "flex",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <Pagination
+                        color="primary"
+                        count={pageCount}
+                        page={page}
+                        onChange={handleChange}
+                      />
+                    </Box>
+                  );
+                },
+              }}
+            />
           </Box>
         ) : (
           <h1>Loading.....</h1>
