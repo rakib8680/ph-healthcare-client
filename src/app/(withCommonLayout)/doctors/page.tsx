f6a6672f82bc42a5eb3fc497a3fa905ec403e4af
rakib8680 <rakib.cst.1st@gmail.com>
2024-07-12 19:51:41 +0600

    Filter Doctors According to their Specialties Implemented

--- src/app/(withCommonLayout)/doctors/page.tsx
+++ src/app/(withCommonLayout)/doctors/page.tsx
@@ -1,48 +1,59 @@
-
-import DashedLine from '@/components/Ui/Doctor/DashedLine';
-import DoctorCard from '@/components/Ui/Doctor/DoctorCard';
-import { Doctor } from '@/types/doctor';
-import { Box, Container } from '@mui/material';
-import React from 'react';
+import DashedLine from "@/components/Ui/Doctor/DashedLine";
+import DoctorCard from "@/components/Ui/Doctor/DoctorCard";
+import ScrollCategory from "@/components/Ui/Doctor/ScrollCategory";
+import { Doctor } from "@/types/doctor";
+import { Box, Container } from "@mui/material";
+import React from "react";
 
 interface PropType {
-   searchParams: { specialties: string };
+  searchParams: { specialties: string };
 }
 
 const Doctors = async ({ searchParams }: PropType) => {
-   let res;
+  let res;
 
-   if (searchParams.specialties) {
-      res = await fetch(
-         `http://localhost:5000/api/v1/doctor?specialties=${searchParams.specialties}`
-      );
-   } else {
-      res = await fetch('http://localhost:5000/api/v1/doctor');
-   }
+  if (searchParams.specialties) {
+    res = await fetch(
+      `http://localhost:5000/api/v1/doctor?specialties=${searchParams.specialties}`
+    );
+  } else {
+    res = await fetch("http://localhost:5000/api/v1/doctor");
+  }
 
-   const { data } = await res.json();
+  const { data } = await res.json();
 
-   // console.log(data);
+  // console.log(data);
 
-   return (
-      <Container sx={{
-        pt:4,
-        pb:20
-      }}>
+  return (
+    <Container
+      sx={{
+        pt: 4,
+        pb: 20,
+      }}
+    >
+      <Box
+        sx={{
+          bgcolor: "#F5F5F5",
+          p: 2,
+          mt: 2,
+          width: "fit-content",
+          borderRadius: 2,
+        }}
+      >
+        <ScrollCategory specialties={searchParams.specialties} />
+      </Box>
 
-         {/* <ScrollCategory specialties={searchParams.specialties} /> */}
+      <Box sx={{ mt: 2, p: 3, bgcolor: "#F5F5F5" }}>
+        {data?.map((doctor: Doctor, index: number) => (
+          <Box key={doctor.id}>
+            <DoctorCard doctor={doctor} />
 
-         <Box sx={{ mt: 2, p: 3, bgcolor: '#F5F5F5' }}>
-            {data?.map((doctor: Doctor, index: number) => (
-               <Box key={doctor.id}>
-                  <DoctorCard doctor={doctor} />
-
-                  {index === data.length - 1 ? null : <DashedLine />}
-               </Box>
-            ))}
-         </Box>
-      </Container>
-   );
+            {index === data.length - 1 ? null : <DashedLine />}
+          </Box>
+        ))}
+      </Box>
+    </Container>
+  );
 };
 
 export default Doctors;
